28fb66b563831098bab3b11018b5e8ae4ca46fe7
lovely.fernandez@students.fhnw.ch <lovely.fernandez@students.fhnw.ch>
2025-02-18 09:19:23 +0000

    week 02 lab completed - learning about seek

---
+++ games_project_labs/boid.gd
@@ -0,0 +1,34 @@
+class_name Boid extends CharacterBody3D
+
+var v:Vector3
+var acceleration:Vector3
+var force:Vector3
+
+@export var max_speed:float=10
+@export var mass:float=1
+
+@export var target_node_path:NodePath
+
+#var t = $"../Target"
+var target_pos
+
+func _ready():
+	var t = get_node(target_node_path)
+	#var t = $"../Target"
+	target_pos = t.position
+	
+func seek(target):
+	var to_target:Vector3 = target - position
+	var desired = to_target.normalized() * max_speed
+	return desired - v
+
+func _physics_process(delta):
+	force = seek(target_pos)
+	acceleration = force / mass
+	v += acceleration * delta
+	
+	var speed = v.length()
+	if speed > 0:
+		look_at(position - v)
+	move_and_collide(v * delta)
+	
